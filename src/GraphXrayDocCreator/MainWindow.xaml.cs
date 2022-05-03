310c7c4040f873c2e431bee9db202dba0c75488f
Merill Fernando <merill@merill.net>
2022-05-03 18:28:33 +1000

    Initial commit

---
+++ src/GraphXrayDocCreator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Web.WebView2.Core;
+
+
+namespace WPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            webView.SourceChanged += WebView_SourceChanged;
+            InitializeAsync();
+        }
+
+        private void WebView_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            addressBar.Text = webView.Source.ToString();
+        }
+
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            addressBar.Text = uri;
+        }
+
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(addressBar.Text);
+            }
+        }
+
+    }
+
+}
+
